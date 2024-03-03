5160c5c4c3d286a3b796d5213f190baeeffc550e
Artom Shevchenko <87131438+ShevchenkoArtom@users.noreply.github.com>
2024-03-03 18:15:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Некоторые элементы
+const checkAnswers = document.querySelector(".learnResult-btn");
+const inputs = document.querySelectorAll(".inputAnswer");
+let score = 0;
+
+let isGotValues = [];
+
+checkAnswers.addEventListener("click", function() {
+    const inLenth = inputs.length;
+    
+    for (let i = 0; i < inLenth; i++) {
+        isGotValues[i] = 
+    }
+
+
+});
+
+// Правильные ответы
+const trueAnswers1 = {
+    ex1: 35,
+    ex2: "wzyx",
+    ex3: 3570,
+}
